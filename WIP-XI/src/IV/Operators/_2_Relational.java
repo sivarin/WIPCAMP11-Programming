109bd842ba4679492c1920b08bcf98eb0bab3a2b
sivarin <51091712+sivarin@users.noreply.github.com>
2019-05-31 08:43:50 +0700

    Add project

---
+++ WIP-XI/src/IV/Operators/_2_Relational.java
@@ -0,0 +1,27 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _2_Relational {
+    public static void main(String[] args) {
+        int x = 1;
+        
+        System.out.println("== : " + (x == 2));
+        
+        System.out.println("!= : " + (x != 3));
+        
+        System.out.println("< : " + (x < 1));
+        
+        System.out.println("> : " + (x > 9));
+        
+        System.out.println("<= : " + (x <= 12));
+        
+        System.out.println("<= : " + (x <= 8));
+    }
+}
